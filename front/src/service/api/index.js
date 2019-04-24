8311f3d1c5b07c95a4db98b06617b22ad08b1e7b
krry <krryxq@163.com>
2019-04-24 16:41:14 +0800

    style(note): add api nodes

--- front/src/service/api/index.js
+++ front/src/service/api/index.js
@@ -4,48 +4,124 @@ import qs from 'qs';
 export default {
   // 实际上项目名称就叫 krryblog，而 Tomcat 设置默认首页就是 krryblog，所以 api 接口的访问路径也可以不需要加上 krryblog，默认匹配上 krryblog 这个项目
   // 而 api 接口这里访问路径前面加上 krryblog，是为了 proxyTable 代理的匹配路径统一为 krryblog
+
+  /**
+   * 获取博客列表
+   * @param {*} reqData
+   */
   fetchBlog (reqData) {
     return Ax.get('/krryblog/blog/getBlog', {params: reqData});
   },
+
+  /**
+   * 获取博客详情页
+   * @param {*} id
+   */
   fetchBlogDetail (id) {
     return Ax.get(`/krryblog/blog/getBlogDetail/${id}`);
   },
+
+  /**
+   * 获取博客分类名称
+   */
   fetchClassify () {
     return Ax.get('/krryblog/blog/getClassify');
   },
+
+  /**
+   * 根据分类 id 获取博客列表
+   * @param {*} id
+   * @param {*} reqData
+   */
   fetchBlogByClassifyId (id, reqData) {
     return Ax.get(`/krryblog/blog/getBlogByClassifyId/${id}`, {params: reqData});
   },
+
+  /**
+   * 添加博客
+   * @param {*} reqData
+   */
   addBlog (reqData) {
     return Ax.post('/krryblog/blog/addBlog', qs.stringify(reqData));
   },
+
+  /**
+   * 修改博客
+   * @param {*} reqData
+   */
   updateBlog (reqData) {
     return Ax.post('/krryblog/blog/updateBlog', qs.stringify(reqData));
   },
+
+  /**
+   * 修改博客评论数 +1
+   * @param {*} reqData
+   */
   updateCommonBlog (reqData) {
     return Ax.post('/krryblog/blog/updateCommonBlog', qs.stringify(reqData));
   },
+
+  /**
+   * 删除博客封面
+   * @param {*} id
+   * @param {*} reqData
+   */
   deleteBlogCover (id, reqData) {
     return Ax.post(`/krryblog/blog/deleteBlogCover/${id}`, qs.stringify(reqData));
   },
+
+  /**
+   * 获取《相关链接》或《关于》博客
+   * @param {*} reqData
+   */
   fetchLinkOrAbout (reqData) {
     return Ax.get('/krryblog/krry/getLinkOrAbout', {params: reqData});
   },
+
+  /**
+   * 获取编辑博客时的详情页（包含 markdown 字段）
+   * @param {*} id
+   */
   fetchEditBlogDetail (id) {
     return Ax.get(`/krryblog/krry/getBlogDetail/${id}`);
   },
+
+  /**
+   * 根据博客标签获取博客列表
+   * @param {*} reqData
+   */
   fetchBlogByTag (reqData) {
     return Ax.get(`/krryblog/part/getBlogByTag`, {params: reqData});
   },
+
+  /**
+   * 根据关键词搜索获取博客列表
+   * @param {*} reqData
+   */
   fetchBlogByKeyword (reqData) {
     return Ax.get(`/krryblog/part/getBlogBykeyword`, {params: reqData});
   },
+
+  /**
+   * 登录
+   * @param {*} reqData
+   */
   fetchLogin (reqData) {
     return Ax.get(`/krryblog/krry/login`, {params: reqData});
   },
+
+  /**
+   * 修改用户信息
+   * @param {*} reqData
+   */
   updateUser (reqData) {
     return Ax.post(`/krryblog/krry/updateUser`, qs.stringify(reqData));
   },
+
+  /**
+   * 获取所有博客
+   * @param {*} reqData
+   */
   fetchAllBlog (reqData) {
     return Ax.get(`/krryblog/krry/getBlog`, {params: reqData});
   },
