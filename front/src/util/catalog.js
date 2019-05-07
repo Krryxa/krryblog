7965c89ae165dcfa003fcfba22360984ca4264fa
krry <krryxq@163.com>
2019-05-07 17:41:17 +0800

    perf(category): style and code

--- front/src/util/catalog.js
+++ front/src/util/catalog.js
@@ -1,6 +1,13 @@
 'use strict';
 
-var _typeof = typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol' ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === 'function' && obj.constructor === Symbol && obj !== Symbol.prototype ? 'symbol' : typeof obj; };
+var _typeof = typeof Symbol === 'function' &&
+  typeof Symbol.iterator === 'symbol' ? function (obj) {
+    return typeof obj;
+  } : function (obj) {
+    return obj && typeof Symbol === 'function' &&
+      obj.constructor === Symbol &&
+      obj !== Symbol.prototype ? 'symbol' : typeof obj;
+  };
 
 export default function (opts) {
   var defaultOpts = {
@@ -20,8 +27,10 @@ export default function (opts) {
   // var $scrollWrap = Opt.scrollWrapper; // 滚动条区域
   var $catalog = document.getElementById(Opt.catalogEl); // 目录容器
 
-  // 在内容区获取要生成目录的 H 标签，allCatalogs：数组形式
-  var allCatalogs = $content.querySelectorAll(Opt.selector.join());
+  // querySelectorAll() 返回所匹配到的所有元素集合；数组：NodeList[]
+  // 在内容区获取要生成目录的 H 标签元素数组，allCatalogs：nodeList[]数组形式
+  var allCatalogs = $content.querySelectorAll(Opt.selector.join()); // Opt.selector.join() => "h1,h2,h3"
+
   // 生成目录树
   var tree = getCatalogsTree(allCatalogs);
 
@@ -33,7 +42,7 @@ export default function (opts) {
 
   // 事件注册
   $catalog.addEventListener('click', clickHandler);
-  Opt.cool || window.addEventListener('scroll', simpleScrollHandler);
+  window.addEventListener('scroll', simpleScrollHandler);
 
   /**
    * 滚动事件
@@ -42,7 +51,7 @@ export default function (opts) {
     var scrollToEl = null;
     for (var i = allCatalogs.length - 1; i >= 0; i--) {
       var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-      if (allCatalogs[i].offsetTop <= scrollTop + 80) {
+      if (allCatalogs[i].offsetTop <= scrollTop + 30) {
         scrollToEl = allCatalogs[i];
         break;
       }
@@ -67,6 +76,7 @@ export default function (opts) {
 
   /**
    * 获取目录树，生成类似于Vnode的树
+   * catalogs NodeList[]
    * @param catalogs
    */
   function getCatalogsTree (catalogs) {
@@ -79,19 +89,20 @@ export default function (opts) {
     var id = void 0;
 
     for (var i = 0; i < catalogs.length; i++) {
-      title = catalogs[i].innerText || catalogs[i].textContent;
-      tagName = catalogs[i].tagName;
+      title = catalogs[i].innerText || catalogs[i].textContent; // 获取当前 H 标签的内容名称
+      tagName = catalogs[i].tagName; // 获取 H 标签的标签名
       id = 'heading-' + i;
-      catalogs[i].id = id;
+      catalogs[i].id = id; // 给内容区域的 H 标签设置 id
       treeItem = {
         name: title,
         tagName: tagName,
         id: id,
-        level: +getLevel(tagName),
+        level: +getLevel(tagName), // H 标签等级
         parent: parentItem,
       };
       if (lastTreeItem) {
-        if (getLevel(treeItem.tagName) > getLevel(lastTreeItem.tagName)) {
+        // 当前 treeItem 的 H > 上一个 treeItem 的 H （如 3 > 2）H3 是 H2 的子标题，说明当前 treeItem.parent = 上一个 treeItem
+        if (treeItem.level > lastTreeItem.level) {
           treeItem.parent = lastTreeItem;
         } else {
           treeItem.parent = findParent(treeItem, lastTreeItem);
@@ -111,14 +122,16 @@ export default function (opts) {
    */
   function findParent (currTreeItem, lastTreeItem) {
     var lastTreeParent = lastTreeItem.parent;
-    while (lastTreeParent && getLevel(currTreeItem.tagName) <= getLevel(lastTreeParent.tagName)) {
+    // 找到 lastTreeParent 的 H 标签是 currTreeItem 的父标签就停止循环
+    // 如：H3 是 H2 的子标题，当 currTreeItem H3 > lastTreeParent H2 就找到了父级 H2
+    while (lastTreeParent && currTreeItem.level <= lastTreeParent.level) {
       lastTreeParent = lastTreeParent.parent;
     }
     return lastTreeParent || { id: -1 };
   }
 
   /**
-   *  获取等级
+   *  获取 H 标签的等级，就是获取 H1 中的 1 / H2 中的 2
    * @param tagName
    * @returns {*}
    */
@@ -127,7 +140,7 @@ export default function (opts) {
   }
 
   /**
-   * 生成DOM树
+   * 生成 DOM 树
    * @param tree
    * @param _parent
    * @return {string}
@@ -141,7 +154,7 @@ export default function (opts) {
         if (isEqual(tree[i].parent, _parent)) {
           hasChild = true;
           ul += '<li><div class=\'' + Opt.linkClass + ' cl-level-' + tree[i].level + '\' ' + Opt.datasetName + '=\'' + tree[i].id + '\'>' + tree[i].name + '</div>';
-          ul += generateHtmlTree(tree, tree[i]);
+          ul += generateHtmlTree(tree, tree[i]); // 递归获取子节点
           ul += '</li>';
         }
       }
@@ -154,7 +167,10 @@ export default function (opts) {
    * 判断是否是相同节点
    */
   function isEqual (node, node2) {
-    return node && node2 && (typeof node === 'undefined' ? 'undefined' : _typeof(node)) === 'object' && (typeof node2 === 'undefined' ? 'undefined' : _typeof(node2)) === 'object' && node.id === node2.id;
+    return node && node2 &&
+      (typeof node === 'undefined' ? 'undefined' : _typeof(node)) === 'object' &&
+      (typeof node2 === 'undefined' ? 'undefined' : _typeof(node2)) === 'object' &&
+      node.id === node2.id;
   }
 
   /**
@@ -167,7 +183,7 @@ export default function (opts) {
 
     catas.forEach(function (T) {
       if (T.getAttribute(Opt.datasetName) === id) {
-        typeof Opt.activeHook === 'function' && !T.classList.contains(Opt.linkActiveClass) && Opt.activeHook.call(_this, T); // 执行active钩子
+        typeof Opt.activeHook === 'function' && !T.classList.contains(Opt.linkActiveClass) && Opt.activeHook.call(_this, T); // 执行 active 钩子
         T.classList.add(Opt.linkActiveClass);
       } else {
         T.classList.remove(Opt.linkActiveClass);
@@ -176,6 +192,7 @@ export default function (opts) {
   }
 };
 
+// 复制数组
 function _toConsumableArray (arr) {
   if (Array.isArray(arr)) {
     for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
